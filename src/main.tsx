42be36289117d2e63bbc08def6e4cc33feb80a31
LiuYiBest <1070692044@qq.com>
2025-03-18 23:29:56 +0800

    React Router v6

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,24 @@
 import React from "react"
 import ReactDOM from "react-dom/client"
-import App from "./App.tsx"
+import { RouterProvider } from "react-router-dom"
+import { ConfigProvider } from "antd"
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools"
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query"
+import zhCN from "antd/locale/zh_CN"
+import { router } from "./routes"
 import "antd/dist/reset.css"
 import "./index.css"
 import "./utils/dayjs" // 引入 dayjs 配置
 
+const queryClient = new QueryClient()
+
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
+    <QueryClientProvider client={queryClient}>
+      <ConfigProvider locale={zhCN}>
+        <RouterProvider router={router} />
+        <ReactQueryDevtools initialIsOpen={false} />
+      </ConfigProvider>
+    </QueryClientProvider>
   </React.StrictMode>
 )
